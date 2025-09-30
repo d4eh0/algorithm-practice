8f632f6da1822bd92a103cec79426682411dc722
d4eh0 <eogud3332@naver.com>
2025-10-01 00:16:10 +0900

    feat: 문자찾기

---
+++ 개념공부/String/문자찾기/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String text = br.readLine().toLowerCase();
+        char target = br.readLine().toLowerCase().charAt(0);
+        int count = 0;
+
+        for(int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == target ) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
