cd0dd8b81c130608cc9dccc883ec0c8108ac269b
d4eh0 <eogud3332@naver.com>
2025-10-05 01:51:18 +0900

    feat: 가장짧은문자거리

--- 개념공부/String/가장짧은문자거리/Main.java
+++ 개념공부/String/가장짧은문자거리/Main.java
@@ -1,4 +1,37 @@
 package 가장짧은문자거리;
 
+import java.util.*;
+import java.io.*;
+
 public class Main {
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        char[] text = st.nextToken().toCharArray();
+        ArrayList<Integer> arr = new ArrayList<>();
+        char target = st.nextToken().charAt(0);
+
+        // 타겟찾기
+        for (int i = 0; i < text.length; i++) {
+            if (text[i] == target) {
+                arr.add(i);
+            }
+        }
+
+        // 거리계산
+        for (int i = 0; i < text.length; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j : arr) {
+                if ( Math.abs(i - j) < min)
+                    min = Math.abs(i - j);
+            }
+            sb.append(min);
+            sb.append(' ');
+        }
+        System.out.println(sb.toString().trim());
+    }
 }
