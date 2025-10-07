a324afe1a75b3a3477436edeb241790b0491f356
d4eh0 <eogud3332@naver.com>
2025-10-08 02:42:34 +0900

    feat: 암호

--- 개념공부/String/암호/Main.java
+++ 개념공부/String/암호/Main.java
@@ -1,4 +1,52 @@
 package 암호;
 
+import java.util.*;
+import java.io.*;
+
+// substring, replace, parseInt(String s, int radix)
 public class Main {
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String text = br.readLine();
+
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(
+                    text.substring(0, 7).replace('#', '1').replace('*', '0'), 2);
+            System.out.print((char) a);
+            text = text.substring(7);
+        }
+    }
 }
+
+//public class Main {
+//    public static void main(String args[]) throws Exception {
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        int count = Integer.parseInt(br.readLine());
+//        String text = br.readLine();
+//
+//        for (int i = 0; i < count; i++) {
+//            char[] arr = new char[7];
+//            for (int j = 0; j < 7; j++) {
+//                arr[j] = text.charAt(i * 7 + j);
+//            }
+//            System.out.print((char) toDecimal(arr));
+//        }
+//    }
+//
+//    public static int toDecimal(char[] arr) {
+//        int i = 6;
+//        int result = 0;
+//        for (char c : arr) {
+//            if (c == '#')
+//                result += Math.pow(2, i);
+//            i--;
+//        }
+//        return result;
+//    }
+//}
