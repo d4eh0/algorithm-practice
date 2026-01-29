034249a4b5d6df6da471709c6cd40e4607093745
Park Dae Hyeong <eogud3332@naver.com>
2026-01-30 02:54:57 +0900

    [Bronze V] Title: 채플이 너무해, Time: 104 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.java
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 34945                             :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: eogud3332 <boj.kr/u/eogud3332>              +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/34945                          #+#        #+#      #+#    */
+/*   Solved: 2026/01/30 02:52:36 by eogud3332     ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		if (Integer.parseInt(br.readLine()) >= 6)
+			System.out.println("Success!");
+		else
+			System.out.println("Oh My God!");
+
+	}
+}
