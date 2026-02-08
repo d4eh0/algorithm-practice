af7d95983c9807262160bbc16658ae83ef40fd96
Park Dae Hyeong <eogud3332@naver.com>
2026-02-08 21:59:06 +0900

    [Bronze II] Title: 분해합, Time: 108 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 2231                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: eogud3332 <boj.kr/u/eogud3332>              +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/2231                           #+#        #+#      #+#    */
+/*   Solved: 2026/02/08 20:31:26 by eogud3332     ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			int cur = i;
+			int result = i;
+			while (cur > 0) {
+				result += cur % 10;
+				cur /= 10;
+			}
+			if (result == N) {
+				System.out.println(i);
+				return;
+			}
+		}
+		System.out.println(0);
+	}
+}
